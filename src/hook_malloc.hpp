7c1e2578b900a236808f549abec962a61a923688
tdavidcl <timothee.davidcleri@gmail.com>
2024-09-17 09:49:21 +0200

    add a backtrace

--- src/hook_malloc.hpp
+++ src/hook_malloc.hpp
@@ -1,12 +1,12 @@
 #include "cuda_hook.hpp"
 #include <cstdio>
-
+#include "backtrace.hpp"
 
 inline CUresult hooked_cuMemAlloc(CUdeviceptr *dptr, size_t bytesize){
 
     auto res = real_cuMemAlloc(dptr, bytesize);
     printf("-- cuMemAlloc %p %lu\n", *dptr, bytesize);
-
+    print_bt();
     return res;
 }
 
@@ -14,6 +14,6 @@ inline CUresult hooked_cuMemFree(CUdeviceptr dptr){
 
     auto res = real_cuMemFree(dptr);
     printf("-- cuMemFree %p\n", dptr);
-
+    print_bt();
     return res;
 }
